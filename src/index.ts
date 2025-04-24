bdc079999dfceb9e1fd0c70cfa0eaea4be1af71c
Favour Enang <fenang@softalliance.co.ng>
2025-04-24 23:18:47 +0100

    Setting up for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import Kernel from './kernel';
-import Logger from './services/Logger';
+// import Logger from './services/Logger';
 
-const PORT = Kernel.get('PORT');
+// const PORT = Kernel.get('PORT');
 
-Kernel.listen(Kernel.get('PORT'), () => {
-  Logger.info(`server is runing on PORT localhost:${PORT}`);
-});
+// Kernel.listen(Kernel.get('PORT'), () => {
+//   Logger.info(`server is runing on PORT localhost:${PORT}`);
+// });
+
+export const app = Kernel
+
+export * from './decorators';
+export * from './providers';
+export * from './utils'
